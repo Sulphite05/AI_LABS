5f47e97afd6ea94cfa77ee314a590354b1f3fa96
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-10-09 22:22:17 +0500

    Questions

---
+++ AI Assignment/Back Propogation.py
@@ -0,0 +1,6 @@
+# Write an algorithm for back propagation training on multilayer feedforward network. The algorithm
+# may take initial weight values, learning rate and input table from user (input table may be two input
+# table of any logical gate). The algorithm must be capable of running perceptron learning algorithm
+# and train the model. After training the user may be able to feed testing inputs and the algorithm
+# may be able to generate output approximating the function. [Marks: 1]
+
---
+++ AI Assignment/Perceptron.py
@@ -0,0 +1,5 @@
+# Write down a program for training a perceptron. The algorithm may take initial weight values,
+# learning rate and input table from user (input table may be two input table of any logical gate). The
+# algorithm must be capable of running perceptron learning algorithm and train the model. After
+# training the user may be able to feed testing inputs and the algorithm may be able to generate output
+# approximating the function. Show the practical demonstration to course instructor.
